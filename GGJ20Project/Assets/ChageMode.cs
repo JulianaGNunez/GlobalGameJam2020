1be785c4a46a9ded3e98a5b3ae8df08121fe3c32
Luis <felipe.paiser@gmail.com>
2020-02-01 08:08:39 -0300

    Feito codigo para alternar entre gameplays de pipes e fps.

---
+++ GGJ20Project/Assets/ChageMode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mode : MonoBehaviour{
+    public void Swap(string newMode) {
+        //"PipeMode" e "CharMode" sao substituidos pelo nome da classe dos controles de cada modo de jogo
+        if(newMode == "3d"){
+            FindObjectOfType<PipeMode>().enabled = false;
+            FindObjectOfType<CharMode>().enabled = true;
+        } else if(newMode == "2d"){
+            FindObjectOfType<PipeMode>().enabled = true;
+            FindObjectOfType<CharMode>().enabled = false;
+        }
+    }
+}
