af81070ed0d4af83f7348d69328cea65bea03e37
Elerson <elersonpalermo@hotmail.com>
2020-02-01 12:07:10 -0300

    random na geração das tiles, controle do selector

--- GGJ20Project/Assets/Scripts/PipeManager.cs
+++ GGJ20Project/Assets/Scripts/PipeManager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PipeManager : MonoBehaviour
 {
-    public int gridSizex, gridSizeY;
+    public int gridSizeX, gridSizeY;
 
     int currentWaterIndexX = 0, currentWaterIndexY = 0;
 
-    public GameObject pipePrefab;
+    public GameObject[] pipePrefab;
 
     //public GameObject pipesHolder;
 
@@ -16,32 +16,67 @@ public class PipeManager : MonoBehaviour
 
     public List<Vector2> randomPositions;
 
-    private Vector2 GetPositionFinish(Vector2 pos){
+    float bufferInput = 0;
+    float bufferInput_max = 0.1f;
+
+    Vector2 selectedTile_pos = new Vector2();
+
+    public GameObject selectedTile_selector;
+    public GameObject selectedTile_marker;
+
+    private Vector2 GetPositionFinish(Vector2 pos)
+    {
         randomPositions.Remove(pos);
         return pos;
     }
 
-    private Vector2 GetRandomPosition(){
+    private Vector2 GetRandomPosition()
+    {
         int randomIndex = (int)Random.Range(0, randomPositions.Count);
         Vector2 pos = randomPositions[randomIndex];
         randomPositions.Remove(pos);
         return pos;
     }
+    private void Start()
+    {
+        LayOutLevel();
+    }
+
+    private void Update()
+    {
+        if (bufferInput <= bufferInput_max)
+            bufferInput += Time.deltaTime;
+
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        if (movement.sqrMagnitude > 0 && bufferInput > bufferInput_max)
+        {
+            bufferInput = 0;
+            //if ((selectedTile_pos + movement).x <= gridSizeX && (selectedTile_pos + movement).y <= gridSizeY)
+            //{
+                selectedTile_pos += movement * 100;
+                selectedTile_selector.transform.localPosition = selectedTile_pos;
+            //}
+        }
+        
+
+    }
 
     void LayOutLevel()
     {
-        int objectsLeft = gridSizex * gridSizeY;
+        int objectsLeft = gridSizeX * gridSizeY;
         GameObject pipeObject;
-        for(int i = 0; i < gridSizex; ++i){
+        for (int i = 0; i < gridSizeX; ++i)
+        {
             for (int j = 0; j < gridSizeY; ++j)
             {
-                randomPositions.Add(Vector2(i, j));
+                randomPositions.Add(new Vector2(i, j));
 
                 // Deveria estar lá embaixo
-                if(true){
-                    pipeObject = Instantiate(pipePrefab);
-                    pipeObject.transform.parent = this.transform;
-                    pipeObject.transform.localPosition = new Vector3(i, j, 0);
+                if (true)
+                {
+                    pipeObject = Instantiate(pipePrefab[Random.Range(0, pipePrefab.Length - 1)]);
+                    pipeObject.transform.SetParent(this.transform);
+                    pipeObject.transform.localPosition = new Vector3(i * 100, j * 100, 0);
                 }
             }
         }
@@ -51,4 +86,11 @@ public class PipeManager : MonoBehaviour
 
         // Colocar full random
     }
+
+    void MoveTiles()
+    {
+
+    }
+
+
 }
--- GGJ20Project/Assets/Scripts/ScriptablePipe.cs
+++ GGJ20Project/Assets/Scripts/ScriptablePipe.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PipeData", menuName = "ScriptableObjects/PipeData", order = 1)]
 public class ScriptablePipe : ScriptableObject
 {
-    public Sprite tileSprite;
+    public GameObject pipePrefab;
     public Pipe.Rotation objectRotation = Pipe.Rotation.zero;
     public int pipeDirections;
     public bool endPipe = false;
