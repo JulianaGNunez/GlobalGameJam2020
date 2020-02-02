00fe68b9f134b84daf517f4d4b9b09a4c10fd2de
Elerson <elersonpalermo@hotmail.com>
2020-02-02 18:48:32 -0300

    fade da tela inicial pra 3D

---
+++ GGJ20Project/Assets/trocaTela.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trocaTela : MonoBehaviour
+{
+  
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            Initiate.Fade("3D", Color.black, 2);
+        }
+    }
+}
